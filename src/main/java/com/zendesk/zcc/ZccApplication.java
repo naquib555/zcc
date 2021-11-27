fcd27453c681b8674e0876c430d64d9342782ad0
naquib555 <naquib555@gmail.com>
2021-11-26 19:18:14 -0500

    remove encryptable configuration

--- src/main/java/com/zendesk/zcc/ZccApplication.java
+++ src/main/java/com/zendesk/zcc/ZccApplication.java
@@ -1,11 +1,9 @@
 package com.zendesk.zcc;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEncryptableProperties
 public class ZccApplication {
 
 	public static void main(String[] args) {
